8c2d7919a903a0277484b4ece85342e64e39c5fb
Tom Bukovczan <tombukowczan@gmail.com>
2018-11-16 21:47:49 +0100

    Kolizje, Anteny

---
+++ Assets/Scripts/AerialScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AerialScript : MonoBehaviour {
+
+    public bool AssembleState;
+    // Use this for initialization
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawCube(this.transform.position, new Vector3(1, 1, 1));
+    }
+
+    void Start () {
+        AssembleState = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
