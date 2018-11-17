58be4c2276a6a09b57fd57b71cbe18182bb9a69e
CatStrategist <catstrategist@gmail.com>
2018-11-17 13:17:33 +0100

    Prepare scene

---
+++ Assets/Scripts/PrepareSceneController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PrepareSceneController: MonoBehaviour {
+    public Image frequencyImage;
+    public int currentIndex;
+    public List<GameObject> texts;
+    public List<AudioClip> clips;
+    public AudioSource audioSource;
+    // Use this for initialization
+    void Start() {
+        StartCoroutine(ShowSequence());
+    }
+
+    IEnumerator ShowSequence() {
+        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(showTextWithAudio(currentIndex) + 0.5f);
+        hideCurrentText();
+        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(showTextWithAudio(currentIndex) + 0.5f);
+        hideCurrentText();
+        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(showTextWithAudio(currentIndex) + 0.5f);
+        hideCurrentText();
+        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(showTextWithAudio(currentIndex) + 0.5f);
+        hideCurrentText();
+        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(showTextWithAudio(currentIndex) + 0.5f);
+        hideCurrentText();
+        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(showTextWithAudio(currentIndex) + 0.5f);
+        hideCurrentText();
+        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(showTextWithAudio(currentIndex) + 0.5f);
+        hideCurrentText();
+        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(showTextWithAudio(currentIndex) + 0.5f);
+        hideCurrentText();
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene("Map");
+
+    }
+
+    private float showTextWithAudio(int index) {
+        texts[index].SetActive(true);
+        audioSource.clip = clips[0];
+        audioSource.Play();
+        return clips[0].length;
+    }
+
+    private void hideCurrentText() {
+        texts[currentIndex].SetActive(false);
+        currentIndex++;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        frequencyImage.fillAmount = (Mathf.PerlinNoise(Time.deltaTime, Time.time) * 2) + 0.2f;
+    }
+}
